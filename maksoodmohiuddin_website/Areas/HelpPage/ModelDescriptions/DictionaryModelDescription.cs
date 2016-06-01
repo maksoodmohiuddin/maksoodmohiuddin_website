5ea47a923337d1ab87bb480439b6236af24b9b6b
Maksood Mohiuddin <maksood@gmail.com>
2016-06-01 00:02:44 -0700

    Setting up re-designed personal website

---
+++ maksoodmohiuddin_website/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace maksoodmohiuddin_website.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ maksoodmohiuddin_website/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace maksoodmohiuddin_website.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
