fc57fec2f93a8dcd9273c3354974600d8a063dbc
Maksood Mohiuddin <maksood@gmail.com>
2016-06-02 23:57:07 -0700

    Updated resume PDF with 1 page as a fix for render issue in mobile browser. Also changed ng-href to ui-sref as a fix for route. Also fixed route change top of page issue.

--- maksoodmohiuddin_website/App/js/app.js
+++ maksoodmohiuddin_website/App/js/app.js
@@ -12,10 +12,14 @@
         'maksoodMohiuddin.educationModule'        
     ]);
 
-    maksoodMohiuddinApp.run(['$state', '$rootScope',
-    function ($state, $rootScope) {
+    maksoodMohiuddinApp.run(['$state', '$rootScope', '$window', 
+    function ($state, $rootScope, $window) {
         // go to default view of the app 
         $state.go('home');
+
+        $rootScope.$on("$stateChangeSuccess", function (event, currentState, previousState) {
+            $window.scrollTo(0, 0);
+        });
     }]);
 
     maksoodMohiuddinApp.config(['$stateProvider', '$locationProvider', '$httpProvider', '$compileProvider',
